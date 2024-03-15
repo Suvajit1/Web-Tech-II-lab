3c28ff91833be27a54e88dc399935b19b76db981
Suvajit Sadhukhan <130191310+Suvajit1@users.noreply.github.com>
2024-03-15 14:12:24 +0530

    Add files via upload

---
+++ Ass15/worker.js
@@ -0,0 +1,20 @@
+function factorial(n) {
+    if(n<0){
+      document.write("no. is invalid");
+    }
+    if (n === 0) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    }
+  }
+  
+  self.addEventListener("message", function(event) {
+    var n = event.data;
+    var table = [];
+    for (var i = 1; i <= n; i++) {
+      table.push(factorial(i));
+    }
+    self.postMessage(table);
+  });
+  
